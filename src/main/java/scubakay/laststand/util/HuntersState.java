ca2b6b29ceb5fa76179e1696d4ccc1104447f7cd
BuildTools <scubakay@gmail.com>
2023-01-18 14:48:10 +0100

    #4 #5 remove bounty when either hunter or target dies

--- src/main/java/scubakay/laststand/util/HuntersState.java
+++ src/main/java/scubakay/laststand/util/HuntersState.java
@@ -1,9 +1,12 @@
 package scubakay.laststand.util;
 
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import scubakay.laststand.item.ModItems;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Keeps list of current hunters.
@@ -17,11 +20,24 @@ public class HuntersState {
         hunters.add(hunterTarget);
     }
 
-    public static boolean removeIfCompleted(ServerPlayerEntity player, ServerPlayerEntity target) {
-        return hunters.removeIf(ht -> ht.hunter.equals(player) && ht.target.equals(target));
+    public static void removeIfPlayerWasHunter(ServerPlayerEntity player) {
+        Optional<HunterTarget> hunterTarget = hunters.stream().filter(ht -> ht.hunter.equals(player)).findFirst();
+        hunterTarget.ifPresent(HuntersState::removeHunterTrackingDevice);
+    }
+
+    public static void removeIfPlayerWasTarget(ServerPlayerEntity player) {
+        List<HunterTarget> hts = hunters.stream().filter(ht -> ht.target.equals(player)).toList();
+        hts.forEach(HuntersState::removeHunterTrackingDevice);
     }
 
     public static void reset() {
         hunters = new ArrayList<>();
+    }
+
+    private static void removeHunterTrackingDevice(HunterTarget ht) {
+        ht.target.sendMessage(Text.translatable("item.laststand.no-longer-being-hunted"));
+        ht.hunter.sendMessage(Text.translatable("item.laststand.bounty-completed"));
+        ht.hunter.getInventory().remove(stack -> stack.isOf(ModItems.HUNTER_TRACKING_DEVICE), 1, ht.hunter.getInventory());
+        hunters.remove(ht);
     }
 }
