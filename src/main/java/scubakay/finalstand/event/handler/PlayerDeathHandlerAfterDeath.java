3326e724e39ba51f47f116893cb1f252245c7fea
Scubakay <=>
2023-06-06 03:10:38 +0200

    #174 Clean up logging

--- src/main/java/scubakay/finalstand/event/handler/PlayerDeathHandlerAfterDeath.java
+++ src/main/java/scubakay/finalstand/event/handler/PlayerDeathHandlerAfterDeath.java
@@ -4,7 +4,6 @@ import scubakay.finalstand.event.ServerLivingEntityEvents;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import scubakay.finalstand.FinalStand;
 import scubakay.finalstand.data.HuntersState;
 import scubakay.finalstand.util.IEntityDataSaver;
 import scubakay.finalstand.data.LivesData;
@@ -21,12 +20,8 @@ public class PlayerDeathHandlerAfterDeath implements ServerLivingEntityEvents.St
     private static void handleBounties(ServerPlayerEntity target, DamageSource damageSource) {
         if (damageSource.getAttacker() instanceof ServerPlayerEntity hunter) {
             HuntersState.removeIfBountyCompleted(hunter, target);
-            FinalStand.LOGGER.info("Damage source attacker is player");
         } else if (target.getPrimeAdversary() instanceof ServerPlayerEntity hunter) {
             HuntersState.removeIfBountyCompleted(hunter, target);
-            FinalStand.LOGGER.info("Prime adversary is player");
-        } else {
-            FinalStand.LOGGER.info("None of those");
         }
     }
 }
