7dc1a2030de0a24b20e822b24ea06488432e4d25
Scubakay <=>
2023-06-06 02:50:45 +0200

    #174 Bounties now also complete when a player was "trying to escape" another player

--- src/main/java/scubakay/finalstand/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/scubakay/finalstand/mixin/ServerPlayerEntityMixin.java
@@ -1,11 +1,16 @@
 package scubakay.finalstand.mixin;
 
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.world.GameMode;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import scubakay.finalstand.event.ServerLivingEntityEvents;
 import scubakay.finalstand.util.IServerPlayerEntity;
 
 @Mixin(ServerPlayerEntity.class)
@@ -16,5 +21,10 @@ public abstract class ServerPlayerEntityMixin implements IServerPlayerEntity {
 
     public boolean fs_isSurvival() {
         return this.interactionManager.getGameMode() == GameMode.SURVIVAL;
+    }
+
+    @Inject(method = "onDeath", at = @At("HEAD"))
+    private void notifyDeath(DamageSource source, CallbackInfo ci) {
+        ServerLivingEntityEvents.START_AFTER_DEATH.invoker().startAfterDeath((ServerPlayerEntity) (Object) this, source);
     }
 }
