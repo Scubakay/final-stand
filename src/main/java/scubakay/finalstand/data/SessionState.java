62777d4b5d0c57c0a22709face78fa3f9f99d2dc
Scubakay <=>
2024-10-28 19:40:49 +0100

    #182 - Update to 1.20.2

--- src/main/java/scubakay/finalstand/data/SessionState.java
+++ src/main/java/scubakay/finalstand/data/SessionState.java
@@ -8,7 +8,7 @@ import net.minecraft.world.World;
 import scubakay.finalstand.FinalStand;
 
 /**
- * <a href="https://fabricmc.net/wiki/tutorial:persistent_states">...</a>
+ * https://fabricmc.net/wiki/tutorial:persistent_states
  */
 public class SessionState extends PersistentState {
     public static final String IN_SESSION_NBT = "inSession";
@@ -59,12 +59,28 @@ public class SessionState extends PersistentState {
         return sessionState;
     }
 
+    private static Type<SessionState> type = new Type<>(
+            SessionState::new, // If there's no 'StateSaverAndLoader' yet create one
+            SessionState::createFromNbt, // If there is a 'StateSaverAndLoader' NBT, parse it with 'createFromNbt'
+            null // Supposed to be an 'DataFixTypes' enum, but we can just pass null
+    );
+
     public static SessionState getServerState(MinecraftServer server) {
-        // First we get the persistentStateManager for the OVERWORLD
+        // (Note: arbitrary choice to use 'World.OVERWORLD' instead of 'World.END' or 'World.NETHER'.  Any work)
         PersistentStateManager persistentStateManager = server.getWorld(World.OVERWORLD).getPersistentStateManager();
 
-        // Calling this reads the file from the disk if it exists, or creates a new one and saves it to the disk
-        // You need to use a unique string as the key. You should already have a MODID variable defined by you somewhere in your code. Use that.
-        return persistentStateManager.getOrCreate(SessionState::createFromNbt, SessionState::new, FinalStand.MOD_ID);
+        // The first time the following 'getOrCreate' function is called, it creates a brand new 'StateSaverAndLoader' and
+        // stores it inside the 'PersistentStateManager'. The subsequent calls to 'getOrCreate' pass in the saved
+        // 'StateSaverAndLoader' NBT on disk to our function 'StateSaverAndLoader::createFromNbt'.
+        SessionState state = persistentStateManager.getOrCreate(type, FinalStand.MOD_ID);
+
+        // If state is not marked dirty, when Minecraft closes, 'writeNbt' won't be called and therefore nothing will be saved.
+        // Technically it's 'cleaner' if you only mark state as dirty when there was actually a change, but the vast majority
+        // of mod writers are just going to be confused when their data isn't being saved, and so it's best just to 'markDirty' for them.
+        // Besides, it's literally just setting a bool to true, and the only time there's a 'cost' is when the file is written to disk when
+        // there were no actual change to any of the mods state (INCREDIBLY RARE).
+        state.markDirty();
+
+        return state;
     }
 }
